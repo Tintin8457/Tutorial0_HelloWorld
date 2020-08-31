545153cfc64a0f2a0f63f6d717acfbe410b5de29
Brandon Perez <70164183+Tintin8457@users.noreply.github.com>
2020-08-30 23:16:08 -0400

    Add files via upload

---
+++ TextController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public Text newText; //Get text GameObject
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Determine if the player is pressing the space
+        if (Input.GetKeyDown("space"))
+        {
+            //The text will change in the game once the player presses the spacebar
+            newText.text = "Hello, my name is Brandon Perez.";
+        }
+
+        //Enable the player to quit the game by pressing escape
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
